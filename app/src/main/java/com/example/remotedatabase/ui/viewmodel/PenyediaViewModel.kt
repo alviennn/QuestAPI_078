5568023c43373a9c91fb9557e08e16c42cfd43f9
alviennn <alvien.rnp@gmail.com>
2024-12-30 05:34:57 +0700

    Menambahkan penyedia ViewModel menggunakan viewModelFactory untuk menyediakan berbagai ViewModel dengan dependensi yang diperlukan

---
+++ app/src/main/java/com/example/remotedatabase/ui/viewmodel/PenyediaViewModel.kt
@@ -0,0 +1,37 @@
+package com.example.remotedatabase.ui.viewmodel
+
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.remotedatabase.MahasiswaApplications
+
+object PenyediaViewModel {
+    val Factory = viewModelFactory {
+        initializer {
+            HomeViewModel(
+                aplikasiMhs().container.mahasiswaRepository
+            )
+        }
+        initializer {
+            InsertViewModel(
+                aplikasiMhs().container.mahasiswaRepository
+            )
+        }
+        initializer {
+            DetailViewModel(
+                aplikasiMhs().container.mahasiswaRepository
+            )
+        }
+        initializer {
+            UpdateViewModel(
+                createSavedStateHandle(),
+                aplikasiMhs().container.mahasiswaRepository
+            )
+        }
+    }
+}
+
+fun CreationExtras.aplikasiMhs(): MahasiswaApplications =
+    (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as MahasiswaApplications)
