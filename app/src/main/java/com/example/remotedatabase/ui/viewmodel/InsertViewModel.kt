abf6530ae1b206fd2e2f6b084bc8068567ea4fc6
alviennn <alvien.rnp@gmail.com>
2024-12-29 12:47:31 +0700

    Menambahkan fungsi untuk mengubah objek InsertUiEvent menjadi Mahasiswa

--- app/src/main/java/com/example/remotedatabase/ui/viewmodel/InsertViewModel.kt
+++ app/src/main/java/com/example/remotedatabase/ui/viewmodel/InsertViewModel.kt
@@ -2,6 +2,15 @@ package com.example.remotedatabase.ui.viewmodel
 
 import com.example.remotedatabase.model.Mahasiswa
 
+fun InsertUiEvent.toMhs(): Mahasiswa = Mahasiswa(
+    nim = nim,
+    nama = nama,
+    alamat = alamat,
+    jenisKelamin = jenisKelamin,
+    kelas = kelas,
+    angkatan = angkatan
+)
+
 fun Mahasiswa.toUiStateMhs(): InsertUiState = InsertUiState(
     insertUiEvent = toInsertUiEvent()
 )
