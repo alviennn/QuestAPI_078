d6f472cbc84b7860113ae766dc3868baa994485b
alviennn <alvien.rnp@gmail.com>
2024-12-30 05:43:00 +0700

    Memanggil MahasiswaApp pada MainActivity

--- app/src/main/java/com/example/remotedatabase/MainActivity.kt
+++ app/src/main/java/com/example/remotedatabase/MainActivity.kt
@@ -12,6 +12,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.remotedatabase.ui.theme.RemoteDatabaseTheme
+import com.example.remotedatabase.ui.viewmodel.MahasiswaApp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,28 +21,11 @@ class MainActivity : ComponentActivity() {
         setContent {
             RemoteDatabaseTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    MahasiswaApp(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    RemoteDatabaseTheme {
-        Greeting("Android")
     }
 }
