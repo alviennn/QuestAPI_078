2509a50a0bab5f41d230094f7cb8c830d76d4d58
alviennn <alvien.rnp@gmail.com>
2024-12-29 13:02:01 +0700

    Menambahkan kelas HomeViewModel untuk mengelola mhsUiState dengan kondisi awal Loading dan memanggil fungsi getmhs() pada inisialisasi

--- app/src/main/java/com/example/remotedatabase/ui/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/example/remotedatabase/ui/viewmodel/HomeViewModel.kt
@@ -1,9 +1,27 @@
 package com.example.remotedatabase.ui.viewmodel
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.remotedatabase.model.Mahasiswa
+import com.example.remotedatabase.repository.MahasiswaRepository
+import kotlinx.coroutines.launch
+import retrofit2.HttpException
+import java.io.IOException
 
 sealed class HomeUiState{
     data class Success(val mahasiswa: List<Mahasiswa>) : HomeUiState()
     object Error : HomeUiState()
     object Loading : HomeUiState()
+}
+
+class HomeViewModel(private val mhs: MahasiswaRepository) : ViewModel(){
+    var mhsUiState: HomeUiState by mutableStateOf(HomeUiState.Loading)
+        private set
+    init {
+        getmhs()
+    }
+
 }
