f77b0b563fc006b898916f2ac81103fa344bff16
alviennn <alvien.rnp@gmail.com>
2024-12-29 12:49:15 +0700

    Menambahkan data class untuk merepresentasikan InsertUiEvent dengan properti nim, nama, alamat, jenisKelamin, kelas, dan angkatan

--- app/src/main/java/com/example/remotedatabase/ui/viewmodel/InsertViewModel.kt
+++ app/src/main/java/com/example/remotedatabase/ui/viewmodel/InsertViewModel.kt
@@ -2,6 +2,15 @@ package com.example.remotedatabase.ui.viewmodel
 
 import com.example.remotedatabase.model.Mahasiswa
 
+data class InsertUiEvent(
+    val nim: String = "",
+    val nama: String = "",
+    val alamat: String = "",
+    val jenisKelamin: String = "",
+    val kelas: String = "",
+    val angkatan: String = ""
+)
+
 fun InsertUiEvent.toMhs(): Mahasiswa = Mahasiswa(
     nim = nim,
     nama = nama,
