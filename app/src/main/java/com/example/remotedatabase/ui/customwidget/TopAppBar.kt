573fde67a2c2e68b5ad49e78dca91080f6e031c1
alviennn <alvien.rnp@gmail.com>
2024-12-29 14:45:15 +0700

    Menambahkan ikon navigasi mundur pada TopAppBar dengan kondisi canNavigateBack

--- app/src/main/java/com/example/remotedatabase/ui/customwidget/TopAppBar.kt
+++ app/src/main/java/com/example/remotedatabase/ui/customwidget/TopAppBar.kt
@@ -2,15 +2,18 @@ package com.example.remotedatabase.ui.customwidget
 
 import androidx.compose.foundation.clickable
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.Refresh
 import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarScrollBehavior
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 
-@OptIn
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun CostumeTopAppBar(
     title: String,
@@ -27,5 +30,13 @@ fun CostumeTopAppBar(
                 onRefresh
             })
         },
+        modifier = modifier,
+        scrollBehavior = scrollBehavior, navigationIcon = {
+            if (canNavigateBack){
+                IconButton(onClick = navigateUp) {
+                    Icon(imageVector = Icons.Filled.ArrowBack, contentDescription = null)
+                }
+            }
+        }
     )
 }
