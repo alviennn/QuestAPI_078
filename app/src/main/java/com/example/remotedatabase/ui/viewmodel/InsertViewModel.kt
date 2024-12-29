c3aba14b36901d8a798ba66f13e921ec573d8d79
alviennn <alvien.rnp@gmail.com>
2024-12-29 12:43:25 +0700

    Menambahkan fungsi untuk mengubah objek Mahasiswa menjadi InsertUiEvent

---
+++ app/src/main/java/com/example/remotedatabase/ui/viewmodel/InsertViewModel.kt
@@ -0,0 +1,13 @@
+package com.example.remotedatabase.ui.viewmodel
+
+import com.example.remotedatabase.model.Mahasiswa
+
+
+fun Mahasiswa.toInsertUiEvent(): InsertUiEvent = InsertUiEvent(
+    nim = nim,
+    nama = nama,
+    alamat = alamat,
+    jenisKelamin = jenisKelamin,
+    kelas = kelas,
+    angkatan = angkatan
+)
